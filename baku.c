666f71f75c3bcbfb40d11b000f14a32eb731538b
Arei1126 <ducati5334@gmail.com>
2022-06-27 00:25:01 +0900

    一応完成

--- baku.c
+++ baku.c
@@ -5,10 +5,9 @@
 #include <unistd.h>
 #include <time.h>
 
-int fileNumber;
-int boxOpen = 0;
+int boxOpen = 0;//スイッチのこと１が開いてる、０が閉じてる
 
-void play(){
+void play(int fileNumber){
 	if(fileNumber == 0){//ファイル数が0なら再生しない
 		return;
 	}
@@ -21,12 +20,32 @@ void play(){
 		sprintf(path, "/usr/bin/aplay ~/baku/%d.wav",playNumber);
 		system(path);
 		}
+	return;
 }
 
-void record(){
+void record(int fileNumber){
+	pid_t pid;
+	char path[63];
+	char effectPath[63];
+	sprintf(path, "~/baku/%d.wav",fileNumber);//録音用のファイル名
+	pid = fork();
+	sprintf(effectPath, "/usr/bin/sox ~/baku/%d.wav ~/baku/%d.wav pitch 400 speed 1.5",fileNumber,fileNumber);//こっちは変換用の文字列
+	switch(pid) {
+		case -1:
+			exit(1);
+		case 0:
+			while(boxOpen == 1);//閉じてから動き出す
+			kill(pid, SIGINT);//録音するプロセスを殺して
+			system(effectPath);//変換する
+		default :
+			execl("/usr/bin/arecord", "arecord","-D","plughw:2,0","-f","cd",path,NULL);//子プロセスで録音
+	}
+	return;
+
 }
 
 int main(){
+	int fileNumber;//これが扱うファイル名になる
 	FILE *fp;
 
 	fp = fopen("baku_record","r+");
@@ -37,10 +56,11 @@ int main(){
 	fileNumber = fgetc(fp);
 	printf("%d",fileNumber);
 	while(1){
-		if(boxOpen = 1){
-			record();
+		if(boxOpen == 1){
+			fileNumber++;
+			record(fileNumber);
 		}
-		play();
+		play(fileNumber);
 	}
 }	
 
