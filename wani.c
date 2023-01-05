2aee58fc366f3edd460e5ec9174d83c5d6f3cbc8
Arei1126 <ducati5334@gmail.com>
2023-01-05 16:27:28 +0900

    For EbisuOtoko

--- wani.c
+++ wani.c
@@ -17,11 +17,32 @@
 #define NO_STOP 0
 
 //再生時のインターバル(秒)
-#define INTERVAL 30 
+#define INTERVAL 30
+
+#define BT_OR_HUG 1  // 1でボタン、0でハグ
+#define WEIT 10  //  録音秒数
 
 //int boxOpen = NO_ACTION;//スイッチのこと１が開いてる、０が閉じてる
 FILE *fp;//録音の一覧用のファイルポインタ
 FILE *fp2;
+
+#バリエーションのためのカウンタ
+char Calling = 0;
+char Greeting = 0;
+char Explaination = 0;
+char Rec = 0;
+char Reaction = 0;
+
+//  カウンタが循環するようにする
+#define CHK_MAX(counter, max)	((counter) > (max) ? 0 : (counter))
+
+#define CALLING_MAX 4
+#define GREETING_MAX 6
+#define EXPLAINATION_MAX 3
+#define REC_MAX 2
+#define REACTION_MAX 4
+
+
 
 //指定秒数だけ止まる関数
 int recordWait(time_t duration) {
@@ -50,7 +71,7 @@ int chkStop(){
 }
 
 void play(int fileNumber){
-	//pid_t pid;
+	pid_t pid;
 
 	//printf("entering playing func\n");
 	if(fileNumber == 0){//ファイル数が0なら再生しない
@@ -60,24 +81,39 @@ void play(int fileNumber){
 	int playNumber;
 	time_t nowTime = time(NULL);
 	char path[1024];
+	char call[1024];
 	nowTime = (int)nowTime;
 	if((nowTime % INTERVAL) == 0){ //30秒に一回再生
-		playNumber = rand() % fileNumber + 1;
-		sprintf(path, "/usr/bin/aplay %d.wav",playNumber);
-		printf("Play:%d\n",playNumber);
-		system(path);
+		playNumber = rand() % fileNumber + 1;  // ランダムに一つ選ぶ
+
+		//  呼びかけ用の部分
+		sprintf(call, "/usr/bin/splay call_%d.wav", Calling);
+		Calling++;  // 
+		CHK_MAX(Calling, CALLING_MAX);  
+
+		sprintf(path, "/usr/bin/aplay %d.wav",playNumber);  // 再生用のコマンド
+
+
+	//	printf("Play:%d\n",playNumber);
+	//	system(path);
 	
-	/*	pid = fork();
+		pid = fork();
 		switch(pid) {
 			case -1:
 				exit(-1);
 			case 0://In Child Process
-				system(path);
+				system(call);  //  よびかけ再生
+				system(path);  //  録音した音声を再生
 			default://In Parent Process
-				if(chkAction() == ACTION)
+				while (1) {  //  再生中に何かるかどうか待つ。中断するため
+					if(kill(pid, 0) != 0)  // 存在しなければ抜ける
+						break;
+					if(chkAction() == ACTION)  // アクションがあったら子を殺す
+						kill(pid, SIGKILL);
+				}
+
 
 		}
-		*/
 	return;
 	}
 }
@@ -86,14 +122,43 @@ void record(int fileNumber){
 	printf("entering recording function\n");
 
 //録音前の音
-	system("/usr/bin/aplay /home/naohiro/baku/baku/soundeffect/recStartZundaOhako.wav");
+	
+//	system("/usr/bin/aplay /home/naohiro/baku/baku/soundeffect/recStartZundaOhako.wav");
 
 	pid_t pid;
 	char path[1024];
 	char effectPath[1024];
 	char playPath[1024];
+
+	char greeting[1024];
+	char explaination[1024];
+	char recStart[1024];
+	char recStop[1024];
+	char reaction[1024];
+	
+	//ここから反応のバリエーションを増やすための処理が入る 
+	sprintf(greeting, "/usr/bin/aplay greet_%d.wav",Greeting);  //  あいさつ
+	Greeting++;
+	CHK_MAX(Greeing, GREETING_MAX);
+
+	sprintf(recStart, "/usr/bin/aplay recStart_%d.wav",Rec);
+	sprintf(recStop, "/usr/bin/aplay recStop_%d.wav",Rec);
+	Rec++;
+	CHK_MAX(Rec, REC_MAX);
+
+	sprintf(explaination, "/usr/bin/aplay explain_%d.wav",Explaination);
+	Explaination++;
+	CHK_MAX(Explaination, EXPLAINATION_MAX);
+
+	sprintf(reaction, "/usr/bin/aplay react_%d.wav",Reaction);
+	Reaction++;
+	CHK_MAX(Reaction, REACTION_MAX);
+	////////////////////////////////////////////////////////
+	system(greeting);  // あいさつ
+	system(explaination);  // 説明
+	system(recStart);  // 録音開始の合図
+
 	sprintf(path, "%d_original.wav",fileNumber);//録音用のファイル名
-	//printf("File name is %s\n",path);
 	sprintf(effectPath, "/usr/bin/sox %d_original.wav %d.wav silence 1 5 10%% norm pitch 600",fileNumber,fileNumber);//Converting Strings for Muon
 	//printf("Effect path is %s\n",effectPath);
 	//sprintf(effectPath, "/usr/bin/sox %d_original.wav %d.wav norm pitch 250 speed 1.25",fileNumber,fileNumber);//こっちは変換用の文字列
@@ -116,14 +181,23 @@ void record(int fileNumber){
 			printf("recording PID:%d\n",pid);
 
 			//while(boxOpen() == ACTION);//閉じてから動き出す
-			//recordWait((time_t)10);//時間経過で抜ける
-			while(chkAction() == ACTION); //持ち上げている間はループして待つ
+			
+			if(BT_OR_HUG == 1)
+			{
+				recordWait((time_t)WEIT);//時間経過で抜ける
+			}
+			else
+			{
+				while(chkAction() == ACTION); //持ち上げている間はループして待つ
+			}
 
 			printf("Killing record\n");
 			kill(pid, SIGINT);//録音するプロセスを殺して
 			printf("Recording finished\n");
 			
 			//system("/usr/bin/aplay /home/naohiro/baku/baku/soundeffect/recStop.wav");
+			system(recStop);  // 録音終了の合図
+			system(reaction);  // ちょっとした反応を見せる
 			printf("Starting Effect\n");
 			system(effectPath);//変換する
 			system(playPath);//Oumu Gaeshi
